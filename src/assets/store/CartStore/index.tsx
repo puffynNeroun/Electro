88a1902c554d9406e7e71fd1c2a84f60eed41f07
oleksandrkhmil <oleksandr.khmil@sumup.com>
2023-05-08 13:04:07 +0300

    added CartStore

--- src/assets/store/CartStore/index.tsx
+++ src/assets/store/CartStore/index.tsx
@@ -1,7 +1,55 @@
 import { makeAutoObservable } from 'mobx';
 
-export default class CartStore {
+// продукт который кладут в корзину
+export type Product = {
+  id: string,
+  name: string,
+  image: string,
+  price: number,
+}
+
+// вещь которую уже положили в корзину
+export type CartItem = Product & {
+  count: number,
+}
+
+export class CartStore {
+  // вещи в корзине
+  items: CartItem[] = []
+
   constructor() {
     makeAutoObservable(this)
+  }
+
+  // добавить продукт в корзину
+  add(item: Product): this {
+    // проверяем есть ли в корзине
+    const exists = this.items.some(x => x.id == item.id)
+
+    if (exists) {
+      // если существует - то увеличиваем на 1 количество
+      this.items = this.items
+        .map(x => x.id == item.id ? { ...x, count: x.count + 1 } : x)
+    } else {
+      // если нет - то добавляем в конец
+      this.items = [...this.items, { ...item, count: 1 }]
+    }
+
+    // возвращаем this чтобы можно было писать cart.add({}).add({}).add({})
+    return this
+  }
+
+  // удалить продукт из корзины по идентификатору
+  delete(id: string): this {
+    this.items = this.items
+      .map(x => x.id == id ? { ...x, count: x.count - 1 } : x)
+      .filter(x => x.count > 0)
+    return this
+  }
+
+  // очистить корзину
+  clear(): this {
+    this.items = []
+    return this
   }
 }
