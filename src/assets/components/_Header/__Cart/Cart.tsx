34346d9aebe4a1918bb2f32a4ad4009e5b07e92e
oleksandrkhmil <oleksandr.khmil@sumup.com>
2023-05-08 13:15:05 +0300

    added cart view logic

--- src/assets/components/_Header/__Cart/Cart.tsx
+++ src/assets/components/_Header/__Cart/Cart.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import product01 from '../../../img/product01.png';
-import product02 from '../../../img/product02.png';
+import { useContext, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faClose, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
+import { StoreContext } from '../../../../context';
 
 function Cart() {
+    const { cart } = useContext(StoreContext);
+    const totalCount = cart.items.reduce((sum, item) => sum + item.count, 0) // Всего вещей
+    const totalSum = cart.items.reduce((sum, item) => sum + item.count * item.price, 0)
+
     const [isOpen, setIsOpen] = useState(false);
 
     const handleToggle = () => setIsOpen(!isOpen);
@@ -20,33 +23,27 @@ function Cart() {
 
             {isOpen && (
                 <div className="cart-dropdown">
+                    {/* Список корзины */}
                     <div className="cart-list">
-                        <div className="product-widget">
-                            <div className="product-img">
-                                <img src={product01} alt="" />
+                        {cart.items.map(item => (
+                            <div className="product-widget">
+                                <div className="product-img">
+                                    <img src={item.image} alt="" />
+                                </div>
+                                <div className="product-body">
+                                    <h3 className="product-name"><a href={`#${item.id}`}>{item.name}</a></h3>
+                                    <h4 className="product-price"><span className="qty">{item.count}x</span>${(item.price / 100).toFixed(2)}</h4>
+                                </div>
+                                <button className="delete"><FontAwesomeIcon icon={faClose} className="fa fa-close" /></button>
                             </div>
-                            <div className="product-body">
-                                <h3 className="product-name"><a href="#">Product Name Here</a></h3>
-                                <h4 className="product-price"><span className="qty">1x</span>$980.00</h4>
-                            </div>
-                            <button className="delete"><FontAwesomeIcon icon={faClose} className="fa fa-close" /></button>
-                        </div>
-
-                        <div className="product-widget">
-                            <div className="product-img">
-                                <img src={product02} alt="" />
-                            </div>
-                            <div className="product-body">
-                                <h3 className="product-name"><a href="#">Product Name Here</a></h3>
-                                <h4 className="product-price"><span className="qty">3x</span>$980.00</h4>
-                            </div>
-                            <button className="delete"><FontAwesomeIcon icon={faClose} className="fa fa-close" /></button>
-                        </div>
+                        ))}
                     </div>
 
+                    {/* Результат */}
                     <div className="cart-summary">
-                        <small>Selected 3 item(s)</small>
-                        <h5>SUMMARY: $2940.00</h5>
+                        {/* <small>Selected 3 item(s)</small> */}
+                        <small>Selected {cart.items.length} item(s). Total items {totalCount}</small>
+                        <h5>SUMMARY: ${(totalSum / 100).toFixed(2)}</h5>
                     </div>
 
                     <div className="cart-btns">
